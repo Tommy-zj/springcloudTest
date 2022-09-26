74cd29409bdf405f9a94782db89852ef97fff6e4
jjsimida <1825893557@qq.com>
2022-09-26 11:47:54 +0800

    nacos热更新

--- cloud-client/src/main/java/com/zhao/demo/cloudclient/controller/HelloController.java
+++ cloud-client/src/main/java/com/zhao/demo/cloudclient/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.zhao.demo.cloudclient.controller;
 
 import com.zhao.demo.cloudclient.service.HelloService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +17,19 @@ import javax.annotation.Resource;
  */
 @RestController
 @RequestMapping("/hello")
+@RefreshScope
 public class HelloController {
+
+
+    @Value("${pattern.dateformat}")
+    private String dateformat;
+
     @Resource
     private HelloService helloService;
 
-    @GetMapping("get")
+    @GetMapping("")
     public String get() {
+        System.out.println("获取到的nacos配置中心的配置>>>" + dateformat);
         return helloService.getHello();
     }
 
